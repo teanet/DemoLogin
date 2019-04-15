db9c0cd68447e3c0b322afda132606651e39f0fd
e.tyutyuev <e.tyutyuev@2gis.ru>
2019-04-15 12:41:47 +0700

    Initial Commit

---
+++ DemoLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoLogin
+//
+//  Created by Eugene Tutuev on 15/04/2019.
+//  Copyright © 2019 Eugene Tutuev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
